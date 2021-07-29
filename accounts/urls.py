5bea5a7dee38a3b04611808d4d3bdda61301e8ae
Habibur Rahaman Bhuiyan <habib28296@gmail.com>
2021-07-29 17:48:11 +0600

    password reset feature added

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from .views import SignUp, ProfileView, ProfileUpdateView, ChangePass, user_order, user_order_details
-from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.views import (LoginView, LogoutView, PasswordResetView,
+                                       PasswordResetConfirmView, PasswordResetDoneView,
+                                       PasswordResetCompleteView)
 
 app_name = 'accounts'
 
@@ -14,4 +16,16 @@ urlpatterns = [
     path('profile/change_password', ChangePass.as_view(), name='change_password'),
     path('profile/user_order', user_order, name='order_list'),
     path('profile/<int:pk>/user_order_details', user_order_details, name='order_details'),
+
+    # RESET PASS
+    path('profile/password_reset/', PasswordResetView.as_view(
+        template_name="accounts/password_reset_form.html"), name='password_reset'),
+
+    path('profile/password_reset_done/', PasswordResetDoneView.as_view(
+        template_name="accounts/password_reset_done.html"), name='password_reset_done'),
+
+    path('profile/<uidb64>/<token>/password_reset_confirm/', PasswordResetConfirmView.as_view(
+        template_name="accounts/password_reset_confirm.html"), name='password_reset_confirm'),
+
+    path('profile/password_reset_complete/', PasswordResetCompleteView.as_view(), name='password_reset_complete'),
 ]
--- e_commerce/settings.py
+++ e_commerce/settings.py
@@ -129,3 +129,12 @@ MEDIA_ROOT = MEDIA_DIR
 
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
+
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = '587'
+EMAIL_HOST_USER = 'rahamanhabib2802@gmail.com'
+EMAIL_HOST_PASSWORD = '789789987@gmail'
+EMAIL_USE_TLS = True
+# EMAIL_USE_SSL = False
